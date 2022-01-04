938a2b0dcd2c419581fda6d9aac4e3d9bce09e10
Eduardo Uriel Garza Gonzalez <EDUARDO.URIEL.GARZA.GONZALEZ@AMWAY.COM>
2022-01-04 09:01:52 -0600

    04/01/2022 Sección 10 parte 1

---
+++ bases-vuex/src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Counter - vuex</h1>
+  <h2>Direct access: {{ $store.state.count }}</h2>
+  <!--  <h2>Computed: {{ countComputed }}</h2>-->
+
+  <h1>mapState</h1>
+  <h2>mapState: {{ count }}</h2>
+</template>
+
+<script>
+import { Options, Vue } from "vue-class-component";
+import { mapState }     from "vuex";
+
+@Options( {
+  name    : "Counter",
+  computed: {
+    // ...mapState( [ 'count' ] ),
+    ...mapState( {
+      count: state => state.count
+    } ),
+  }
+} )
+export default class Counter extends Vue {
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ bases-vuex/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Counter />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineAsyncComponent } from 'vue';
+import { Options, Vue }         from 'vue-class-component';
+
+@Options( {
+  components: {
+    Counter: defineAsyncComponent( () => import('@/components/Counter.vue') )
+  }
+} )
+export default class Home extends Vue {}
+</script>
